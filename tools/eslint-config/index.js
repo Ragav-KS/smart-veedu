c407ea33ba18f24c2e680a8e05370dce1c3be46b
Ragav-KS <57057039+Ragav-KS@users.noreply.github.com>
2025-05-16 21:30:07 +0530

    chore(eslint-config): configure Stylistic rules

--- tools/eslint-config/index.js
+++ tools/eslint-config/index.js
@@ -1,8 +1,41 @@
 import eslint from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import { configs as tsESlintConfigs } from 'typescript-eslint';
 
+/**
+ * @type {import('eslint').Linter.Config}
+ */
 export const baseConfig = [
+  {
+    ignores: [
+      'dist/**/*.ts',
+      'dist/**',
+      '**/*.mjs',
+      'eslint.config.mjs',
+      '**/*.js',
+    ],
+  },
   eslint.configs.recommended,
-  ...tsESlintConfigs.strict,
-  ...tsESlintConfigs.stylistic,
+  stylistic.configs.recommended,
+  ...tsESlintConfigs.strictTypeChecked,
+  ...tsESlintConfigs.stylisticTypeChecked,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      parser: tsESlintConfigs.parser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    rules: {
+      '@stylistic/quotes': ['error', 'single'],
+    },
+  },
 ];
